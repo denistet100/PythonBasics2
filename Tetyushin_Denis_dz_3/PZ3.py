236f55c5e725435e77d80455feb3b86aec105a9b
denistet100 <denis02102000@mail.ru>
2021-11-07 22:37:05 +0300

    lesson3_1

--- Tetyushin_Denis_dz_3/PZ3.py
+++ Tetyushin_Denis_dz_3/PZ3.py
@@ -4,30 +4,16 @@
 
 dictionary = { "И":['Иван', 'Илья'], "Д":['Денис', 'Дима'], "М":['Марина'], "Р":['Ринат'], "П":['Пётр'], "О":['Олья']}
 
-#print(sorted(dictionary, key = dictionary.get))
-
 def thesaurus(wrd):
-
-    resalt = dictionary
-
+    buf = ""
+    wrd.sort()
     for i in wrd:
-        
         key = i[0].capitalize() #первая буква
-        
-        #if key not in resalt:
-            #resalt[key] = []
-        #resalt[key].append(i)
-    #return resalt
-
-    #for dictionary.keys(key):
-        print(dictionary.items(key))
-        #print('"', key, ': ', val)
-        #for key in sonted(dictionary.key()):
-            #print('"', key, '": ', val)
-
+        if (buf != key):
+            print('"', key, '": ', dictionary[key])
+        buf = key
 
 names = ["Иван", "Мария", "Петр", "Илья"]
 
-#print(thesaurus(names))
 thesaurus(names)
  
